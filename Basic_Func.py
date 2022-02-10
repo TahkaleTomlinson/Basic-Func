677641dabf94e74ffabebfde708ecc4bb184b0e2
Tahkale Tomlinson <88506269+TahkaleTomlinson@users.noreply.github.com>
2022-02-10 12:27:35 -0600

    Update Basic_Func.py

--- Basic_Func.py
+++ Basic_Func.py
@@ -1,1 +1,9 @@
+User = input("Who is the current user:")
 
+def greet():
+     print("Hello "+ User)
+greet()
+
+day = input("How has your day been:")
+if day == "good":
+    input("Wonderful, What is the Function that you want to use")
