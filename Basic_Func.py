bf346e57bc69f7e7fdcad92a43463ad71e00cf57
Tahkale Tomlinson <88506269+TahkaleTomlinson@users.noreply.github.com>
2022-02-10 13:17:50 -0600

    Update Basic_Func.py

--- Basic_Func.py
+++ Basic_Func.py
@@ -6,4 +6,26 @@ greet()
 
 day = input("How has your day been:")
 if day == "good":
-    input("Wonderful, What is the Function that you want to use")
+    function= input("Wonderful, What is the Function that you want to use")
+if day == "bad":
+    function=input("Sorry to hear that, but maybe a Function would make your day better. What is the Function that you want to use:")
+
+def calc():
+    n1= float(input("What is your first desired number:"))
+    n2= float(input("What is your second desired number:"))
+    operator= input("What is the operator that you want to use:")
+    if operator == "+":
+       answer = n1 + n2
+       print(n1, "+", n2, "=", answer)
+    elif operator == "-":
+       answer = n1 - n2
+       print(n1, "-", n2, "=", answer)
+    elif operator == "/":
+       answer = n1 / n2
+       print(n1, "/", n2, "=", answer)
+    elif operator == "*":
+       answer = n1 * n2
+       print(n1, "*", n2, "=", answer)
+
+if function == "Calc":
+    calc()
