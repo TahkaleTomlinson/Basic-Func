a2d6f3265ffa17c09750f369adc7853a9b0d753e
Tahkale Tomlinson <88506269+TahkaleTomlinson@users.noreply.github.com>
2022-02-10 14:02:21 -0600

    Update Basic_Func.py

--- Basic_Func.py
+++ Basic_Func.py
@@ -1,5 +1,5 @@
-User = input("Who is the current user:")
 
+User = input("Who is the current user:")
 def greet():
      print("Hello "+ User)
 greet()
@@ -14,6 +14,7 @@ def calc():
     n1= float(input("What is your first desired number:"))
     n2= float(input("What is your second desired number:"))
     operator= input("What is the operator that you want to use:")
+
     if operator == "+":
        answer = n1 + n2
        print(n1, "+", n2, "=", answer)
@@ -26,7 +27,7 @@ def calc():
     elif operator == "*":
        answer = n1 * n2
        print(n1, "*", n2, "=", answer)
-       
+
 def date():
     WD = input("What is the wanted date")
     Dates = ["2/10/2022", "2/11/2022", "2/12/2022"]
@@ -36,9 +37,9 @@ def date():
         print(Dates[1]) 
     elif WD == "Tommorrow":
         print(Dates[2])
-      
+
 #Func Caller
 if function == "Calc":
     calc()
 if function == "Date":
-    date()
+    date()  
